4aa6d5e3b4d42e0afcfcf4144ca36456be88f8b3
shubham maithani <shubhammaithani1@gmail.com>
2022-07-28 00:25:04 -0700

    Perfect Number

--- Logicalprog.java
+++ Logicalprog.java
@@ -1,21 +1,33 @@
 package LogicalProgram;
-
+import java.util.Scanner;
 public class Logicalprog {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to logical program");
-		int n = 10, firstTerm = 0, secondTerm = 1;
-		System.out.println("Fibonacci Series till " + n + " terms:");
-
-		for (int i = 1; i <= n; ++i) {
-		  System.out.print(firstTerm + ", ");
-
-		  int nextTerm = firstTerm + secondTerm;
-		  firstTerm = secondTerm;
-		  secondTerm = nextTerm;
-
-
+		{  
+			int n, sum=0;  
+			Scanner sc=new Scanner(System.in);         
+			System.out.print("Enter the number: ");  
+			n=sc.nextInt();  
+			int i=1;  
+			 
+			while(i <= n/2)  
+			{  
+				
+			if(n % i == 0)  
+			{  
+			sum = sum + i;  
+			} 
+		i++;
+			} 
+			
+			if(sum==n)  
+			{  
+		 
+			System.out.println(n+" is a perfect number.");  
+			} 
+			else  
+		     System.out.println(n+" is not a perfect number.");   
+			}  
 		}
-	}
 }
