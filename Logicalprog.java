b14af625f5f3db52854d9ab4076db3262c1329e3
shubham maithani <shubhammaithani1@gmail.com>
2022-07-28 00:33:12 -0700

    Coupon number

--- Logicalprog.java
+++ Logicalprog.java
@@ -2,27 +2,32 @@ package LogicalProgram;
 import java.util.Scanner;
 public class Logicalprog {
 		
-		public static void reverseNumber(int number)   
-		{  
-		if (number < 10)   
-		{  
-		System.out.println(number);  
-		return;  
-		}  
-		else   
-		{  
-		System.out.print(number % 10);  
-		reverseNumber(number/10);  
-		}  
-		}  
-		public static void main(String args[])  
-		{  
-		System.out.print("Enter the number that you want to reverse: ");  
-		Scanner sc = new Scanner(System.in);  
-		int num = sc.nextInt();  
-		System.out.print("The reverse of the given number is: ");   
-		reverseNumber(num);  
-		}  
-	
+	public static void main(String[] args) {
+		 Scanner scr = new Scanner(System.in);
+       System.out.println("Enter the number of coupons to be generated:");
+       int n = scr.nextInt();
 
-	}
+
+
+       char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+       int max = 100000000;
+
+       for (int i = 0; i < n; i++) {
+           int random = (int) (Math.random() * max);
+           StringBuilder sb = new StringBuilder();
+           while (random > 0) {
+               sb.append(chars[random % chars.length]);
+               random /= chars.length;
+
+           }
+
+           String couponCode = sb.toString();
+           System.out.println("Coupon Code: " + couponCode);
+       }
+
+
+   }
+
+
+
+}
