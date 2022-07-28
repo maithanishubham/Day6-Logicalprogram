e383069d27bd8ffffdf5ba46ee5effe6a11f00fa
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 23:50:47 -0700

    Fibonacci Series

--- Logicalprog.java
+++ Logicalprog.java
@@ -4,7 +4,18 @@ public class Logicalprog {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Logical Program");
-	}
+		System.out.println("Welcome to logical program");
+		int n = 10, firstTerm = 0, secondTerm = 1;
+		System.out.println("Fibonacci Series till " + n + " terms:");
 
+		for (int i = 1; i <= n; ++i) {
+		  System.out.print(firstTerm + ", ");
+
+		  int nextTerm = firstTerm + secondTerm;
+		  firstTerm = secondTerm;
+		  secondTerm = nextTerm;
+
+
+		}
+	}
 }
